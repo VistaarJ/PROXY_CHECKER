2c2d6aa9d397160c3a4c38351904befd49bdc272
VistaarJ <vistaarjuneja@yahoo.com>
2016-09-29 00:09:50 +0530

    All checks working perfectly

--- check_all_possible.py
+++ check_all_possible.py
@@ -1,5 +1,8 @@
 import os
 import struct, socket
+import Queue
+import threading
+import urllib2
 
 class bcolors:
     HEADER = '\033[95m'
@@ -11,15 +14,28 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+new_dict = dict()
+
+#store all the current candidates in a map
+myfile=open("candidates.txt")
+read=myfile.read()
+read=read.split()
+for line in read:
+	new_dict[line] = 1
+myfile.close()
+
+#put all the proxy ranges 
 myfile = open("proxy_ranges.txt")
 read = myfile.read()
 read = read.split()
-
+myfile.close()
 
 
 response_url = "https://www.google.com/humans.txt"
 redirect_output_to_file = "temp2.txt"
 google_return_value="Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"
+l=[]
+threads=[]
 
 def inc(str):
 	ip2int = lambda ipstr: struct.unpack('!I', socket.inet_aton(ipstr))[0]
@@ -29,6 +45,8 @@ def inc(str):
 	# ans = lambda n: socket.inet_ntoa(struct.pack('!I', n))
 	return rev
 
+myfile = open("full_list.txt",'w')
+
 for line in read:
 	str = line.split("-")
 	place = str[0]
@@ -37,27 +55,42 @@ for line in read:
 	print(place + starting_add + ending_add)
 	temp = starting_add
 	while(temp!=ending_add):
-		with_port1 = temp + ':' + "3128"
-		comm1 = "curl -s --connect-timeout 0.3 --proxy " + "http://" + with_port1 + " " + response_url + " > " + redirect_output_to_file
-		os.system(comm1)
-		k = open("temp2.txt")
-		output = k.read()
-		# print(output)
-		if(output==google_return_value):
-			print(bcolors.OKGREEN + "Proxy working---->  " + bcolors.ENDC + temp)
+		myfile.write(temp+":3128\n")
+		myfile.write(temp+":8080\n")
 		temp = inc(temp)
-		print(temp)
 
-	temp = starting_add
-	while(temp!=ending_add):
-		with_port2 = temp + ':' + "8080"
-		comm2 = "curl -s --connect-timeout 0.3 --proxy " + "http://" + with_port2 + " " + response_url + " > " + redirect_output_to_file
-		os.system(comm2)
-		k = open("temp2.txt")
-		output = k.read()
-		# print(output)
-		if(output==google_return_value):
-			print(bcolors.OKGREEN + "Proxy working---->  " + bcolors.ENDC + temp)
-		temp = inc(temp)
-		print(temp)
+myfile.close()
 
+myfile = open("full_list.txt")
+read = myfile.read()
+urls = read.split()
+myfile.close()
+
+
+def CheckProxy(s):
+	comm = "curl -s --connect-timeout 1 --proxy " + "http://" + s + " " + response_url
+	# print(comm)
+	var = os.popen(comm).read()
+	if(var==google_return_value):
+		l.append(s)
+		print(bcolors.OKGREEN+s+bcolors.ENDC)
+
+
+for u in urls:
+	t = threading.Thread(target=CheckProxy,args=(u,))
+	t.Daemon=False
+	threads.append(t)
+
+for x in threads:
+	x.start()
+
+for x in threads:
+	x.join()
+
+#Write all the new candidates into candidates.txt
+write_into_candidates = open("candidates.txt",'a')
+for x in l:
+	if not new_dict.has_key(x):
+		write_into_candidates.write(x+"\n")
+	print(bcolors.OKGREEN+"Proxy working ----> " + x+bcolors.ENDC)
+write_into_candidates.close()
